214fa3751cff6596a0b455eb6c813a5a9d0bfba0
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2016-04-06 09:20:31 +0000

    Koncna

--- public/js/script.js
+++ public/js/script.js
@@ -7,14 +7,14 @@ $(document).ready(function() {
     if (url.length > 0)
       page = parseInt(url);
   }
-  
+
   /* requests and sets artist pages */
   $.get('/pages', function(data) {
     for (var i = 0; i < data.pages; i++)
       $('#pages').append(' <a href="/artists/' + (i + 1) + '"><button type="button" ' +
-        'class="btn btn-default' + (i + 1 == page? ' selected': '') + '">' + (i + 1) + '</button></a> ');
+        'class="btn btn-default' + (i + 1 == page ? ' selected' : '') + '">' + (i + 1) + '</button></a> ');
   });
-  
+
   /* requests update of artist's stars on click */
   $('span[stars]').click(function() {
     $.get('/stars/' + $(this).parent().parent().attr('id') + '/' + $(this).attr('stars'), function(data) {
@@ -26,10 +26,17 @@ $(document).ready(function() {
   $('#albums div span').click(function() {
     var details = $(this);
     $.get('/album/' + details.parent().attr('album'), function(data) {
-      details.html(' (' + data.tracks + ' track' + (data.tracks != 1? 's': '') + ' | ' +
+      details.html(' (' + data.tracks + ' track' + (data.tracks != 1 ? 's' : '') + ' | ' +
         Math.round(data.time / 60000) + ' min | <strong>$' + Math.round(data.price) + '</strong> total)');
     });
   });
-  
-  
+
+  $('#playlists div span').click(function() {
+    var details = $(this);
+    $.get('/playlist/' + details.parent().attr('playlist'), function(data) {
+      details.html(' (' + data.artists + ' artist' + (data.artists != 1 ? 's' : '') + ' | ' + data.tracks + ' track' + (data.tracks != 1 ? 's' : '') + ' | <strong>$' + Math.round(data.price) + '</strong> total)');
+    });
+  });
+
+
 });
